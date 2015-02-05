3274cd8ef70c7a97d11219eccf227df7b07a243a
Amol Kulkarni <amol.kulkarni@synerzip.com>
2015-02-05 15:26:56 +0530

    use _id instead of name as unique field

--- app/scripts/services/projects.js
+++ app/scripts/services/projects.js
@@ -10,17 +10,17 @@
 angular.module('dashboardApp')
   .service('projectsService', function () {
         var projects = [
-            {name: 'PDX', openpositions: '6', reddays: '0', billable: 'area-chart.png', peoples: ['Anil'], owner: 'Anil'},
-            {name: 'CMS', openpositions: '0', reddays: '0', billable: 'pie-chart.png', peoples: ['Preshit'], owner: 'Preshit'},
-            {name: 'Loven', openpositions: '1', reddays: '0', billable: 'pie-chart.png', peoples: ['Sumeet','Anuja', 'Vijay'], owner: 'Mukund'},
-            {name: 'LRS', openpositions: '2', reddays: '1', billable: 'pie-chart.png', peoples: ['Sourabh','Rohan'], owner: 'Ashutosh'},
-            {name: 'Google', openpositions: '16', reddays: '4', billable: 'pie-chart.png', peoples: ['Atul','Uttam', 'Rahul'], owner: 'Salil'},
-            {name: 'Mercatus', openpositions: '0', reddays: '2', billable: 'pie-chart.png', peoples: ['Praveen','Mitesh', 'Chandan'], owner: 'Ashutosh'},
-            {name: 'Halliburton', openpositions: '10', reddays: '0', billable: 'pie-chart.png', peoples: ['Mukund','Onkar'], owner: 'Mukund'},
-            {name: 'Zimbra', openpositions: '3', reddays: '0', billable: 'pie-chart.png', peoples: ['hrishikesh','Rane'], owner: 'Ashutosh'},
-            {name: 'Studer', openpositions: '2', reddays: '1', billable: 'pie-chart.png', peoples: ['Preshit'], owner: 'Preshit'},
-            {name: 'CloudOn', openpositions: '0', reddays: '2', billable: 'pie-chart.png', peoples: ['Rohit','Sujit'], owner: 'Amit Bakore'},
-            {name: 'Prezi', openpositions: '4', reddays: '0', billable: 'pie-chart.png', peoples: ['Ravindra', 'Amit'], owner: 'Amit Bakore'}
+            {_id: 1, name: 'PDX', openpositions: '6', reddays: '0', billable: 'area-chart.png', peoples: ['Anil'], owner: 'Anil'},
+            {_id: 2, name: 'CMS', openpositions: '0', reddays: '0', billable: 'pie-chart.png', peoples: ['Preshit'], owner: 'Preshit'},
+            {_id: 3, name: 'Loven', openpositions: '1', reddays: '0', billable: 'pie-chart.png', peoples: ['Sumeet','Anuja', 'Vijay'], owner: 'Mukund'},
+            {_id: 4, name: 'LRS', openpositions: '2', reddays: '1', billable: 'pie-chart.png', peoples: ['Sourabh','Rohan'], owner: 'Ashutosh'},
+            {_id: 5, name: 'Google', openpositions: '16', reddays: '4', billable: 'pie-chart.png', peoples: ['Atul','Uttam', 'Rahul'], owner: 'Salil'},
+            {_id: 6, name: 'Mercatus', openpositions: '0', reddays: '2', billable: 'pie-chart.png', peoples: ['Praveen','Mitesh', 'Chandan'], owner: 'Ashutosh'},
+            {_id: 7, name: 'Halliburton', openpositions: '10', reddays: '0', billable: 'pie-chart.png', peoples: ['Mukund','Onkar'], owner: 'Mukund'},
+            {_id: 8, name: 'Zimbra', openpositions: '3', reddays: '0', billable: 'pie-chart.png', peoples: ['hrishikesh','Rane'], owner: 'Ashutosh'},
+            {_id: 9, name: 'Studer', openpositions: '2', reddays: '1', billable: 'pie-chart.png', peoples: ['Preshit'], owner: 'Preshit'},
+            {_id: 10, name: 'CloudOn', openpositions: '0', reddays: '2', billable: 'pie-chart.png', peoples: ['Rohit','Sujit'], owner: 'Amit Bakore'},
+            {_id: 11, name: 'Prezi', openpositions: '4', reddays: '0', billable: 'pie-chart.png', peoples: ['Ravindra', 'Amit'], owner: 'Amit Bakore'}
         ];
         this.currProjId;
         // AngularJS will instantiate a singleton by calling "new" on this function
@@ -66,7 +66,7 @@ angular.module('dashboardApp')
         var getProjectIndex = function (projects, projId) {
             var len = projects.length;
             for (var idx = 0; idx < len; idx++) {
-                if (projects[idx].name === projId) {
+                if ('' + projects[idx]._id === projId) {
                     return idx;
                 }
             }
