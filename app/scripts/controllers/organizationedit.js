b6e9bb346990fe3f09a18eca0944d978e6081c7b
Jaya Dwivedi <jaya.dwivedi@synerzip.com>
2015-02-06 19:15:04 +0530

    Changes for addOrg, DeleteOrg and UpdateOrg

--- app/scripts/controllers/organizationedit.js
+++ app/scripts/controllers/organizationedit.js
@@ -9,7 +9,7 @@
  */
 angular.module('dashboardApp')
   .controller('OrganizationeditCtrl', function ($scope, $stateParams, organizationsService) {
-
+    $scope.organization = {};
 
         organizationsService.getOrganization($stateParams.orgId)
             .success (function (data){
@@ -19,6 +19,33 @@ angular.module('dashboardApp')
             .error (function (error){
             console.log (error.msg);});
 
-        //$scope.organization = organizationsService.getOrganization($stateParams.orgId);
-        //console.log ($scope.organization.name);
+    $scope.addProject = function () {
+      for(var i = 0; i<= $scope.organization.projects.length-1; i++){
+        if($scope.newProjectName == $scope.organization.projects[i]){
+          alert ("Duplicate Org Name");
+          $scope.newProjectName = undefined;
+          return false;
+        }
+      }
+      if ($scope.newProjectName != undefined && $scope.newProjectName.trim() != '') {
+        organizationsService.addProjectToCurrentOrg($scope.newProjectName.trim(), $scope.organization._id);
+        $scope.$apply();
+        $scope.newProjectName = undefined;
+      }
+    };
+
+    $scope.deleteProject = function(project) {
+
+      organizationsService.removeProject(project, $stateParams.orgId);
+    };
+
+    $scope.updateProject = function(projectIndex, newValue, orgId) {
+      organizationsService.updateProject(projectIndex, newValue, orgId);
+    }
+
+    $scope.resetProject = function(projectIndex, projectName, orgName) {
+      organizationsService.resetProject(projectIndex, projectName, orgName);
+
+      $scope.$apply();
+    }
   });
--- app/scripts/services/organizations.js
+++ app/scripts/services/organizations.js
@@ -58,6 +58,88 @@ angular.module('dashboardApp')
             return response;
         };
 
+        this.addProjectToCurrentOrg = function (newProjectName, orgId) {
+          var currOrg;
+          var idx = getOrganizationIndex (organizations, '' + orgId);
+          if (idx !== -1) {
+            currOrg = organizations[idx];
+          }
+          var newProjects = {projects: currOrg.projects};
+          newProjects.projects.push(newProjectName);
+          $http.put('http://localhost:3000/api/organizations/'+orgId, newProjects)
+            .success(function(item){
+              currOrg = item;
+            })
+            .error(function(error){
+              if (error) {
+              }
+            });
+        };
+
+        this.removeProject = function(deletedProject, orgId) {
+
+          var currOrg;
+          var idx = getOrganizationIndex (organizations, '' + orgId);
+          if (idx !== -1) {
+            currOrg = organizations[idx];
+          }
+          var newProjects = {projects: currOrg.projects};
+          newProjects.projects.splice(newProjects.projects.indexOf(deletedProject), 1);
+          $http.put('http://localhost:3000/api/organizations/'+orgId, newProjects)
+            .success(function(item){
+              currOrg = item;
+            })
+            .error(function(error){
+              if (error) {
+              }
+            });
+        };
+
+    var getProjectIndex = function(projects, projectName) {
+      projects.forEach(function(project, index) {
+        if(project == projectName) {
+          return index;
+        }
+      });
+    };
+
+    this.updateProject = function(projectIndex, project, orgId) {
+      var currOrg;
+      var idx = getOrganizationIndex (organizations, '' + orgId);
+      if (idx !== -1) {
+        currOrg = organizations[idx];
+      }
+      var newProjects = {projects: currOrg.projects};
+      newProjects.projects[projectIndex] = project;
+      $http.put('http://localhost:3000/api/organizations/'+orgId, newProjects)
+        .success(function(item){
+          currOrg = item;
+        })
+        .error(function(error){
+          if (error) {
+          }
+        });
+    };
+
+    this.resetProject = function(projectIndex, projectName, orgId) {
+
+      var currOrg;
+      var idx = getOrganizationIndex (organizations, '' + orgId);
+      if (idx !== -1) {
+        currOrg = organizations[idx];
+      }
+      var newProjects = {projects: currOrg.projects};
+      newProjects.projects[projectIndex] = projectName;
+      $http.put('http://localhost:3000/api/organizations/'+orgId, newProjects)
+        .success(function(item){
+          currOrg = item;
+        })
+        .error(function(error){
+          if (error) {
+          }
+        });
+    };
+
         this.getCurrentOrganization = function () {
             return getOrganization(this.currOrgId);
         };
